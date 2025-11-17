03072faf0b295e8fddfb4b567efacb46f384db67
Patmos <a.mpakaniye@alustudent.com>
2025-11-17 06:14:42 +0400

    Fixed validate amount function and invalid menu options response inthe fourth commit

--- BudgetTracker.py
+++ BudgetTracker.py
@@ -1,4 +1,4 @@
-# I started with this greet_user function so that it will be creating a 
+# I started with this greet_user function so that it will be creating a
 # personalized experience for the user(s)
 # I included the  datetime library because I think the project will involve some kind of time manipulation
 
@@ -18,33 +18,40 @@ def menu():
     print("1. add transactions")
     print("2. list transactions")
     print("3. summarize budget")
-    print("4. view transaction history")
+    print("4. filter transactions")
     print("5. exit")
-    
 # I created the validate_date function so that only valid date inputs can be used for the date of transaction.
-# Otherwise, it loops and prompts the user to put in the correct date. It is also limited between 
-# 2024-2028 and above cause those match the current years we are in.
+# Otherwise, it loops and prompts the user to put in the correct date. It is also limited to the current year
+# we are in which is 2025
 
 def validate_date():
     while True:
         date=input("Enter the date of the transaction (dd/mm/yyyy): \n")
         try:
-            date_object = datetime.strptime(date, "%d/%m/%Y")
-            if date_object.year < 2024:
-                print("Year cannot be below 2024! Please enter a valid year \n")
+            date= datetime.strptime(date, "%d/%m/%Y").date()
+            if date.year < 2025:
+                print("Year cannot be below 2025! Please enter a valid year \n")
                 continue
-            if date_object.year > 2028:
-                print("Year cannot be beyond 2028! Please enter a valid year \n")
+            if date.year > 2025:
+                print("Year cannot be beyond 2025! Please enter a valid year \n")
                 continue
-            return date_object
+            return date
         except ValueError:
             print("Please enter a valid date and year \n")
 
 #I defined the add_transactions function so that it can give the user options to add in their transaction
 def add_transactions():
     t_date= validate_date()
-    amount=input("enter amount of transaction: ")
-    transaction_type=input("enter type of transaction: ")
+    while True:
+        try:
+            amount = (input("enter amount of transaction: "))
+            if int(amount) < 0:
+                print("Invalid amount!! Please input a positive amount \n")
+                continue
+            break
+        except ValueError:
+            print("please enter a valid amount \n")
+    transaction_type=input("enter type of transaction(income/expense): ")
     description=input("enter description of transaction: ")
     transaction = {
         "date": t_date,
@@ -67,13 +74,15 @@ greet_user()
 while True:
     menu()
     choice=int(input("enter your choice: "))
-    if choice==1:
-        add_transactions()
-    elif choice==2:
-        list_transactions()
-    else:
-        print("You entered a wrong choice")
-
+    try:
+        if choice==1:
+            add_transactions()
+        elif choice==2:
+            list_transactions()
+        else:
+            print("please enter a valid choice\n")
+    except ValueError:
+        print("please enter a valid choice\n")
 
 
 
